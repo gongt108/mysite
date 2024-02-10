92bc69451c3172f4b5479aa8f1230d50e72e5a2b
Tiffany Gong <gongt108@gmail.com>
2024-02-10 11:20:12 -0800

    install psycopg

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import psycopg2
+import psycopg
 import dj_database_url
 from dotenv import load_dotenv
 
