55ae32b4ec5dab87ed477e619693cd13e221c85c
Tiffany Gong <gongt108@gmail.com>
2024-02-11 14:24:27 -0800

    set up postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,9 +18,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-conn = psycopg2.connect(
-    dbname="mysite", user="tiffanygong", password="testpass", host="/tmp"
-)
+# conn = psycopg2.connect(
+#     dbname="mysite", user="tiffanygong", password="testpass", host="/tmp"
+# )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,7 +109,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mysite",
+        "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "localhost",
