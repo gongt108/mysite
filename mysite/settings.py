6a5bc86dd3ae11d0f330f077e5e2c11c46c866ee
Tiffany Gong <76094291+gongt108@users.noreply.github.com>
2024-02-13 18:51:10 -0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,7 +50,7 @@ ALLOWED_HOSTS = []
 # using gmail
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "gongt108@gmail.com"
-EMAIL_HOST_PASSWORD = "zfte sxbu ejnt icix "
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
